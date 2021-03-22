f9a5eb8053dcc5f3f149f6b408846af82928f353
shiva <ashiva44@gmail.com>
2021-03-22 12:08:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import HelloWorldJSX from './Hello_world_jsx/Hello_world_jsx.jsx'
-
+import SmComp from './Small_Component/Small_component'
 
 function App() {
   return (
     <div className="App">
          app component
          <HelloWorldJSX/>
+         <SmComp/>
     </div>
   );
 }
---
+++ src/Small_Component/Small_component.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './style.css'
+
+function SmComp(){
+    return (
+        <div className="sm-comp">
+               Small Component with css
+        </div>
+    );
+}
+
+export default SmComp;
